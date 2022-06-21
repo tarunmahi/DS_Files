4e0f1e6593caec09224b220f2cae27409f24c3f9
tarunmahi <tarunmahendra007@gmail.com>
2022-06-21 23:21:39 +0530

    added

--- linkedlist.c
+++ linkedlist.c
@@ -1,1 +1,29 @@
 #include<stdio.h>
+#include<stdlib.h>
+struct Node{
+    int data;
+    struct Node*next;
+
+};
+struct Node*head;
+void insert(int x){
+    Node* temp = (Node*)malloc(sizeof(struct Node));
+    temp->data=x;
+    temp->next=NULL;
+
+}
+
+int main(){
+    head=NULL;
+    int n,i,x;
+    printf("how many elements to add ");
+    scanf("%d",&n);
+    for ( i = 0; i < n; i++)
+    {
+        printf("enter the element");
+        scanf("%d",&x);
+        insert(x);
+        print();
+    }
+    
+}
