66e5befea5774f6c4d0d73ab2934e3719e2b99be
tarunmahi <tarunmahendra007@gmail.com>
2022-07-10 00:33:03 +0530

    add

--- Stack/stackr.cpp
+++ Stack/stackr.cpp
@@ -2,13 +2,14 @@
 #include<cstring>
 using namespace std;
 
-void func(int (*c)[2][3]){
-    printf("%d \n",&c[0][1][1]);
+void func(int (*c)[2][3] ,int (*p)[2]){
+    printf("%d and %d \n",&c[0][1][1],p[0][1]);
     printf("tarun ");
 }
 
 int main(){
     int c[1][2][3] = {{{2,5,13},{7,6,10}}};
-    func(c);
+    int a[2][2]={{12,1},{45,4}};
+    func(c ,a);
 
 }
--- practice/hi.cpp
+++ practice/hi.cpp
@@ -1,21 +1,19 @@
 #include<iostream>
-#include<cstring>
-void print(char *p,int x){
-    while (*p!='\0')
-    {
-        printf("%c ",*(p));
-        printf("\n");
-       
-        p++;
-    }
-     printf("%d ",x);
-
-    
-}
+using namespace std;
 int main(){
-
-    char c[]="TARUN";
-    int siz =strlen(c);
-    print(c,siz);
+    int n;
+    printf("enter size of array \n");
+    scanf("%d ",&n);
+    int *p = (int *)malloc(n*sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        p[i] = i*i;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d",p[i]);
+    }
+    
+    
 
 }
