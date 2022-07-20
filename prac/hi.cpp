0fb0c71abf3fd78f9b75976ef20a5344f162974a
tarunmahi <tarunmahendra007@gmail.com>
2022-07-20 23:14:26 +0530

    added

--- prac/hi.cpp
+++ prac/hi.cpp
@@ -1,23 +1,37 @@
 #include<iostream>
+#include<stdlib.h>
 using namespace std;
-void pprint(){
-    cout<<"helllo world \n";
+void print(int *arr,int val){
+for(int i=0;i<val;i++){
+    cout<<" "<<arr[i];
 }
-int add(int a,int b){
-    int c = a*b;
-    return c;
 }
-void name(char *nam){
-printf("%s ",nam);
+void change (int *ar,int k,int size){
+    int temp;
+    for(int i=0;i<k;i++){
+        temp=ar[size];
+        ar[size]=ar[0];
+        ar[0]=temp;
+    }
+    for(int i=0;i<size;i++){
+        cout<<" "<<ar[i];
+    }
 }
+using namespace std;
 int main(){
-    void(*man)();
-    man=pprint;
-    man();
-    int(*val)(int,int);
-    val=add;
-    printf(" the value is %d ",val(5,2));
-    void(*nas)(char*);
-    nas=name;
-    nas("tom");
+    int size;
+    cout<<"enter the size of array";
+    cin>>size;
+    int a[size];
+    for(int i=0;i<size;i++){
+       cout<<"enter the" <<size<<"elements ";
+       cin>>a[i];
+    }
+    int val = sizeof(a)/sizeof(a[0]);
+    print(a,val);
+    int k;
+    cout<<"enter k value : ";
+    cin>>k;
+    change(a,k,size);
+
 }
