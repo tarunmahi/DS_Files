e15799ac60bf0f1a31d2473d0661dd9b6a0c0c7b
tarunmahi <tarunmahendra007@gmail.com>
2022-07-21 17:43:50 +0530

    added changes

--- trees/bst.cpp
+++ trees/bst.cpp
@@ -1,57 +1,58 @@
 #include<iostream>
 using namespace std;
-struct bstnode{
-    int data;
-    bstnode*left;
-    bstnode*right;
+struct node{
+  int data;
+  node*left;
+  node*right;
 };
-bstnode*newnode(int x){
-    struct bstnode*temp =(struct bstnode*)malloc(sizeof(struct bstnode*));
-    temp->data=x;
-    temp->left=temp->right=NULL;
-    return temp;
+node*newnode(int x){
+  struct node*temp=new node();
+  temp->data=x;
+  temp->left=temp->right=NULL;
+  return temp;
 }
-bstnode* insert(bstnode* root,int x){
-    bstnode*temp=newnode(x);
-    if(root==NULL){
-        root=temp;
+node*insert(node*root,int y){
+  node*temp=newnode(y);
+  if(root==NULL){
+    root=temp;
+  }
+  else if(temp->data<=root->data){
+    root->left=insert(root->left,y);
+  }
+   else if(temp->data>=root->data){
+        root->right=insert(root->right,y);
     }
-    else if(temp->data<=root->data){
-        root->left=insert(root->left,x);
-    }
-    else if(temp->data>=root->data){
-        root->right=insert(root->right,x);
-    }
-    return root;
+  return root;
 }
-bool search(bstnode* root,int num){
-if(root==NULL)return false;
-else if(root->data==num)return true;
-else if(num<=root->data)return(search(root->left,num));
-else return search(root->right,num);
+bool change(node*root,int num){
+  if(root==NULL)return false;
+  else if(root->data==num)return true;
+  else if(num<=root->data)
+    return change(root->left,num);
+  
+  else return change(root->right,num);
 }
-int findmax(bstnode*root){
-    if(root==NULL){
-        cout<<"tree is empty \n";
-        return 0;
-    }
-    while(root->left!=NULL){
-        root=root->left;
-    }
-    return root->data;
-}
+int max(node*root){
+  if(root==NULL)return -1;
+  while(root->right!=NULL){
+    root=root->right;
+  }
+  return root->data;
 
+}
 int main(){
-    bstnode*root=NULL;
-    root=insert(root,50);
-    root=insert(root,5);
-    root=insert(root,25);
-    root=insert(root,11);
-    root=insert(root,17);
-    int num =findmax(root);
-    cout<<"highest value is "<<num;
-
-
-
-
-}
+  node*root=NULL;
+  root=insert(root,12);
+  root=insert(root,2);
+  root=insert(root,1);
+  root=insert(root,19);
+  root=insert(root,120);
+  root=insert(root,15);
+  int num;
+  cout<<"enter the nnumber ";
+  cin>>num;
+  if(change(root,num))cout<<"number found";
+  else cout<<"mnumber not found";
+  int maxval =max(root);
+  cout<<maxval;
+  }
--- trees/prac.cpp
+++ trees/prac.cpp
@@ -1,47 +1,58 @@
 #include<iostream>
 using namespace std;
-struct bstnode{
-    int data;
-    bstnode*left;
-    bstnode*right;
+struct node{
+  int data;
+  node*left;
+  node*right;
 };
-bstnode*getnewnode(int x){
-    bstnode*temp=(struct bstnode*)malloc(sizeof(struct bstnode*));
-    temp->data=x;
-    temp->left=temp->right=NULL;
-    return temp; 
+node*newnode(int x){
+  struct node*temp=new node();
+  temp->data=x;
+  temp->left=temp->right=NULL;
+  return temp;
+}
+node*insert(node*root,int y){
+  node*temp=newnode(y);
+  if(root==NULL){
+    root=temp;
+  }
+  else if(temp->data<=root->data){
+    root->left=insert(root->left,y);
+  }
+   else if(temp->data>=root->data){
+        root->right=insert(root->right,y);
+    }
+  return root;
+}
+bool change(node*root,int num){
+  if(root==NULL)return false;
+  else if(root->data==num)return true;
+  else if(num<=root->data)
+    return change(root->left,num);
+  
+  else return change(root->right,num);
+}
+int max(node*root){
+  if(root==NULL)return -1;
+  while(root->right!=NULL){
+    root=root->right;
+  }
+  return root->data;
 
 }
-bstnode*insert(bstnode*root,int x){
-      bstnode*temp =getnewnode(x);
-      if(root==NULL){
-        root=temp;
-      }
-      else if(temp->data<=root->data){
-        root->left=insert(root->left,x);
-      }
-      else{
-        root->right=insert(root->right,x);
-      }
-      return root;
-}
-bool change(bstnode*root,int num){
-    if(root==NULL)return false;
-    else if(root->data==num)return true;
-    else if(num<=root->data)return(change(root->left,num));
-    else return change(root->right,num);
-}
 int main(){
-struct bstnode *root=NULL;
-root=insert(root,25);
-root=insert(root,255);
-root=insert(root,251);
-root=insert(root,12);
-root=insert(root,5);
-root=insert(root,2);
-cout<<"enter a number to search ";
-int num;
-cin>>num;
-if(change(root,num))cout<<"number is found ";
-else cout<<"number is not found";
-}
+  node*root=NULL;
+  root=insert(root,12);
+  root=insert(root,2);
+  root=insert(root,1);
+  root=insert(root,19);
+  root=insert(root,120);
+  root=insert(root,15);
+  int num;
+  cout<<"enter the nnumber ";
+  cin>>num;
+  if(change(root,num))cout<<"number found";
+  else cout<<"mnumber not found";
+  int maxval =max(root);
+  cout<<maxval;
+  }
