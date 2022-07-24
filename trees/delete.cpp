59e751cf0fbeb556f8f707b65673093e8874fa7f
tarunmahi <tarunmahendra007@gmail.com>
2022-07-24 14:38:53 +0530

    added changes

--- trees/delete.cpp
+++ trees/delete.cpp
@@ -39,6 +39,45 @@ bool isbstutil(node*root,int min,int max){
 bool isbst(node*root){
     return isbstutil(root,INT_MIN,INT_MAX);
 }
+node*findmin(node*root){
+    while(root->left!=NULL){
+        root=root->left;
+    }
+    return root;
+}
+node*delet(node*root,int x){
+   if(root==NULL)return root;
+   else if(x<root->data)root->left=delet(root->left,x);
+   else if(x>root->data)root->right=delet(root->right,x);
+   else{
+    if(root->left==NULL&&root->right==NULL){
+        delete(root);
+        root=NULL;
+    }
+    else if(root->left==NULL){
+        node*temp=root;
+        root=root->right;
+        delete(temp);
+    }
+    else if(root->right==NULL){
+        node*temp=root;
+        root=root->left;
+        delete(temp);
+    }
+    else{
+        node*temp=findmin(root->right);
+        root->data=temp->data;
+        root->right=delet(root->right,temp->data);
+    }
+   }
+   return root;
+}
+void inorder(node*root){
+    if(root==NULL)return;
+    inorder(root->left);
+    cout<<" "<<root->data;
+    inorder(root->right);
+}
 int main(){
     node*root=NULL;
       root=insert(root,15);
@@ -51,8 +90,7 @@ int main(){
              root=insert(root,10);
               root=insert(root,33);
                root=insert(root,123);
+    root=delet(root,76);
 
-
-    if(isbst(root))cout<<"balanced";
-    else cout<<"not balanced";
+    inorder(root);
 }
