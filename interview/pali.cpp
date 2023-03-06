26d3be2c8b0127fe0438aa5013daa4857ac1916f
tarunmahi <tarunmahendra007@gmail.com>
2023-03-06 20:00:57 +0530

    practice

--- interview/pali.cpp
+++ interview/pali.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 
 int main(){
+    int num,r,sum=0,temp;
+    cout<<"enter num :";
+    cin>>num;
+    temp=num;
+    while (num>0)
+    {
+        r=num%10;
+        sum=(sum*10)+r;
+        num=num/10;
+    }
     
-    
-
-
+    if(temp==sum)cout<<"prime";
+    else cout<<"not a prime";
 }
