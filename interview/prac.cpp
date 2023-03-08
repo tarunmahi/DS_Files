0fd3dbda9b788572a2018ab2958e54c32d4f4c74
tarunmahi <tarunmahendra007@gmail.com>
2023-03-08 11:54:18 +0530

    binary search

--- interview/prac.cpp
+++ interview/prac.cpp
@@ -1,15 +1,38 @@
 #include<iostream>
 
 using namespace std;
-int main(){
-    char val[]={'T','A','R','U','N'};
-    int tot=sizeof(val)/sizeof(val[0]);
-    string a="";
-    for (int i = 0; i <  tot; i++)
-    {
-        a=a+val[i];
-    }
-    cout<<a<<" ";
-    
 
+
+int output(int arr[],int n,int key){
+ for (int i = 0; i < n; i++)
+ {
+    if(arr[i]==key){
+        return i;
+    }
+    
+ }return -1;
+}
+int binary(int arr[],int n,int key){
+ int s=0,e=n;
+ int mid=(s+e)/2;
+ if(arr[mid]==key){
+    return mid;
+ }else if(arr[mid]>key){
+    e=mid-1;
+ }else{
+    s=mid+1;
+ }
+}
+int main(){
+    int n;
+    int key;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+    cout<<"enter key";
+    cin>>key;
+
+    cout<<binary(arr,n,key);
 }
